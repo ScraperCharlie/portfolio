6d414e3ed1473b5fecd0f158587d24b5c1d3ac59
Rishav Chandra <146986198+ScraperCharlie@users.noreply.github.com>
2024-02-02 23:58:45 +0530

    Add files via upload

---
+++ common/footer.js
@@ -0,0 +1,6 @@
+document.write(`<footer class="background footer">
+<p class="text-footer">
+copyright © 2027 - All right reserved Design &amp; Developed By Rishav Chandra</p>
+</footer>
+</body>
+</html>`)
---
+++ common/header.js
@@ -0,0 +1,38 @@
+document.write(`<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Css/style.css">
+    <link rel="stylesheet" href="/Css/service_style.css">
+    <link rel="stylesheet" href="/Css/style_about.css">
+    <link rel="stylesheet" href="/Css/contact_us.css">
+    <link rel="stylesheet" href="/Css/blog.css">
+
+    <title>My Portfolio</title>
+</head>
+
+<body>
+
+    <!-- Navigation Section -->
+
+    <nav class="navbar background">
+        <div class="logo">
+            <img src="/img/Black & White Minimalist Business Logo.png" alt="Logo">
+        </div>
+        <ul class="nav-list another">
+
+            <li><a href="index.html">Home</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li><a href="about.html">About Me</a></li>
+            <li><a href="contact.html">Contact Us</a></li>
+        </ul>
+
+        <div class="rightnav">
+            <input type="text" name="search" id="search">
+            <button class="btn btn-sm">Search</button>
+        </div>
+    </nav>`)
+    
